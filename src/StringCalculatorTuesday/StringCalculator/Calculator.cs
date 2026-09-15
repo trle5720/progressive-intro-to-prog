6c265dda97f2acc8d3d81f4b2401ce37c4d17dca
trle5720 <blankie126@gmail.com>
2026-09-15 14:18:01 -0600

    calc returns converted single integer

--- src/StringCalculatorTuesday/StringCalculator/Calculator.cs
+++ src/StringCalculatorTuesday/StringCalculator/Calculator.cs
@@ -3,6 +3,14 @@ public class Calculator
 {
     public int Add(string numbers)
     {
-        return numbers == "" ? 0 :1;
+
+        if ((numbers != "") && (int.TryParse(numbers, out int x)))
+        {
+            return x;
+        }
+        else
+        {
+            return 0;
+        }
     }
 }
--- src/StringCalculatorTuesday/StringCalculator/CalculatorTests.cs
+++ src/StringCalculatorTuesday/StringCalculator/CalculatorTests.cs
@@ -12,4 +12,17 @@ public class CalculatorTests
 
         Assert.Equal(0, result);
     }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("3", 3)]
+    [InlineData("4", 4)]
+    public void SingleDigit(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+        var answer = calculator.Add(numbers);
+
+        Assert.Equal(expected, answer);
+    }
 }
